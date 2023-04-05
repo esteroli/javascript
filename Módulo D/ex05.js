7a0f7004eb1a5df9804fac61214577432272f098
esteroli <ester.oli.souz4@gmail.com>
2023-04-05 11:02:22 -0300

    Update ex05.js

--- Módulo D/ex05.js
+++ Módulo D/ex05.js
@@ -1,10 +1,9 @@
-var idade = 67
+var idade = 35
+console.log(`Você tem ${idade} anos.`)
 if (idade < 16) {
     console.log('Não vota.')
-} else if (idade < 18) {
+} else if (idade < 18 || idade > 65) {
     console.log('Voto opcional.')
 } else if (idade >= 18) {
     console.log('Voto obrigatório.')
-} else if (idade >= 65) {
-    console.log('Voto opcional.')
 }
