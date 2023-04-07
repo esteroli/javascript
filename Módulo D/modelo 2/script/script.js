abf38e55e8f5b551f7d35ad0fd07ad41f62421c7
esteroli <ester.oli.souz4@gmail.com>
2023-04-07 13:28:22 -0300

    Update modelo 2

--- Módulo D/modelo 2/script/script.js
+++ Módulo D/modelo 2/script/script.js
@@ -9,11 +9,46 @@ function verificar() {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
         if (fsex[0].checked) {
             genero = 'Mulher'
+            if (idade >= 0 && idade < 5) {
+                // bebe
+                img.setAttribute('src', 'imagens/bebe-f.png')
+            } else if (idade < 13) {
+                // criança
+                img.setAttribute('src', 'imagens/crianca-f.png')
+            } else if (idade < 21) {
+                // jovem
+                img.setAttribute('src', 'imagens/jovem-f.png')
+            } else if (idade < 50) {
+                // adulto
+                img.setAttribute('src', 'imagens/adulto-f.png')
+            } else {
+                // idoso
+                img.setAttribute('src', 'imagens/idosa-f.png')
+            }
         } else if (fsex[1].checked) {
             genero = 'Homem'
+            if (idade >= 0 && idade < 5) {
+                // bebe
+                img.setAttribute('src', 'imagens/bebe-m.png')
+            } else if (idade < 13) {
+                // criança
+                img.setAttribute('src', 'imagens/crianca-m.png')
+            } else if (idade < 21) {
+                // jovem
+                img.setAttribute('src', 'imagens/jovem-m.png')
+            } else if (idade < 50) {
+                // adulto
+                img.setAttribute('src', 'imagens/adulto-m.png')
+            } else {
+                // idoso
+                img.setAttribute('src', 'imagens/idoso-m.png')
+            }
         }
         res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
