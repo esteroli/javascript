2357e9531ace07cf9103254f44850ff2c1a08a57
esteroli <ester.oli.souz4@gmail.com>
2023-04-12 13:44:31 -0300

    Modified modelo 1

--- Módulo E/modelo 1/script/script.js
+++ Módulo E/modelo 1/script/script.js
@@ -4,16 +4,27 @@ function contar() {
     let pas = document.getElementById('txtp')
     let res = document.getElementById('res')
 
-    if(ini.value.length == 0 || fim.value.length == 0) {
+    if(ini.value.length == 0 || fim.value.length == 0 || pas.value.length == 0) {
         window.alert('[ERRO] Faltam dados!')
     } else {
         res.innerHTML = 'Contando: '
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(pas.value)
-
-        for(let c = i; c <= f; c += p){
-            res.innerHTML += `${c} ➡️ `
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando passo 1')
+            p = 1
+        }
+        if (i < f){
+            // contagem crescente
+            for(let c = i; c <= f; c += p){
+                res.innerHTML += `${c} ➡️ `
+            }
+        } else {
+            // contagem decrescente
+            for(let c = i; c>= f; c -= p) {
+                res.innerHTML += `${c} ➡️ `
+            }
         }
         res.innerHTML += `🏁`
     }
