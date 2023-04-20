9046b4e19f2a3f021016978f9401fb1c57788ffa
esteroli <ester.oli.souz4@gmail.com>
2023-04-20 17:30:45 -0300

    Updates

--- Módulo F/modelo 1/script/script.js
+++ Módulo F/modelo 1/script/script.js
@@ -25,8 +25,36 @@ function add() {
         let item = document.createElement('option')
         item.text = `Valor ${num.value} adicionado`;
         lista.appendChild(item);
+        res.innerHTML = ''
     } else {
         window.alert('Valor inválido ou já encontrado na lista.')
     }
+    num.value = ''
+    num.focus()
 }
-// console.log(add)
+
+function fim() {
+    if (valores.length == 0) {
+        window.alert = 'Adicione valores antes de finalizar!'
+    } else {
+        let total = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if (valores[pos] < menor)
+                menor = valores[pos]
+        }
+        media = soma / total
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${total} número cadastrados.</p>`
+        res.innerHTML += `<p>O menor número é ${menor}.</p>`
+        res.innerHTML += `<p>O maior número é ${maior}.</p>`
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`
+        res.innerHTML += `<p>A média dos valores digitados é ${media}.</p>`
+    }
+}
